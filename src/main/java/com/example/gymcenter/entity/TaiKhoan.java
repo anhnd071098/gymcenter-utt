200d933c533a7f1d27f2a40cde615535156b08f4
Nguyen Anh <anh98it@gmail.com>
2021-04-16 15:54:00 +0700

    Sửa entity tên đang nhập là gmail

--- src/main/java/com/example/gymcenter/entity/TaiKhoan.java
+++ src/main/java/com/example/gymcenter/entity/TaiKhoan.java
@@ -26,9 +26,6 @@ public class TaiKhoan {
     @Column(name = "email")
     private String email;
 
-    @Column(name = "tenTaiKhoan")
-    private String tenTaiKhoan;
-
     @Column(name = "matKhau")
     private String matKhau;
 
@@ -47,14 +44,13 @@ public class TaiKhoan {
     public TaiKhoan() {
     }
 
-    public TaiKhoan(Integer ID, String tenNguoiDung, Integer tuoi, String diaChi, String soDienThoai, String email, String tenTaiKhoan, String matKhau, Date ngayCapNhat, Integer trangThaiThanhToan, Integer trangThaiThue, Integer IDAdmin) {
+    public TaiKhoan(Integer ID, String tenNguoiDung, Integer tuoi, String diaChi, String soDienThoai, String email, String matKhau, Date ngayCapNhat, Integer trangThaiThanhToan, Integer trangThaiThue, Integer IDAdmin) {
         this.ID = ID;
         this.tenNguoiDung = tenNguoiDung;
         this.tuoi = tuoi;
         this.diaChi = diaChi;
         this.soDienThoai = soDienThoai;
         this.email = email;
-        this.tenTaiKhoan = tenTaiKhoan;
         this.matKhau = matKhau;
         this.ngayCapNhat = ngayCapNhat;
         this.trangThaiThanhToan = trangThaiThanhToan;
@@ -62,13 +58,16 @@ public class TaiKhoan {
         this.IDAdmin = IDAdmin;
     }
 
-    public TaiKhoan(String tenNguoiDung, Integer tuoi, String diaChi, String soDienThoai, String email, String tenTaiKhoan, String matKhau) {
+    public TaiKhoan(String tenNguoiDung, Integer tuoi, String diaChi, String soDienThoai, String email) {
         this.tenNguoiDung = tenNguoiDung;
         this.tuoi = tuoi;
         this.diaChi = diaChi;
         this.soDienThoai = soDienThoai;
         this.email = email;
-        this.tenTaiKhoan = tenTaiKhoan;
+    }
+
+    public TaiKhoan(String email, String matKhau) {
+        this.email = email;
         this.matKhau = matKhau;
     }
 
@@ -76,47 +75,87 @@ public class TaiKhoan {
         return ID;
     }
 
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
     public String getTenNguoiDung() {
         return tenNguoiDung;
+    }
+
+    public void setTenNguoiDung(String tenNguoiDung) {
+        this.tenNguoiDung = tenNguoiDung;
     }
 
     public Integer getTuoi() {
         return tuoi;
     }
 
+    public void setTuoi(Integer tuoi) {
+        this.tuoi = tuoi;
+    }
+
     public String getDiaChi() {
         return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
     }
 
     public String getSoDienThoai() {
         return soDienThoai;
     }
 
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
     public String getEmail() {
         return email;
     }
 
-    public String getTenTaiKhoan() {
-        return tenTaiKhoan;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getMatKhau() {
         return matKhau;
     }
 
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
     public Date getNgayCapNhat() {
         return ngayCapNhat;
+    }
+
+    public void setNgayCapNhat(Date ngayCapNhat) {
+        this.ngayCapNhat = ngayCapNhat;
     }
 
     public Integer getTrangThaiThanhToan() {
         return trangThaiThanhToan;
     }
 
+    public void setTrangThaiThanhToan(Integer trangThaiThanhToan) {
+        this.trangThaiThanhToan = trangThaiThanhToan;
+    }
+
     public Integer getTrangThaiThue() {
         return trangThaiThue;
     }
 
+    public void setTrangThaiThue(Integer trangThaiThue) {
+        this.trangThaiThue = trangThaiThue;
+    }
+
     public Integer getIDAdmin() {
         return IDAdmin;
+    }
+
+    public void setIDAdmin(Integer IDAdmin) {
+        this.IDAdmin = IDAdmin;
     }
 }
